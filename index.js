ac966c03db36bc4b0bf14733968588326f5d7cb3
Luca Matteis <lmatteis@gmail.com>
2018-10-10 01:46:53 +0200

    Add some example

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function* Button() {
   this.updateView(
     <button
       onClick={() => {
-        this.request('BUTTON_CLICKED', 'ciao');
+        this.request(
+          'BUTTON_CLICKED',
+          'ciao'
+        );
       }}
     >
       Click me twice!
@@ -49,7 +52,44 @@ const threads = [
 
 ReactDOM.render(
   <Provider threads={threads}>
-    React Behavioral
+    <h2>react-behavioral</h2>
+    <pre>yarn add react-behavioral</pre>
+    <p>
+      <a href="https://twitter.com/search?q=%23BehavioralProgramming">
+        #BehavioralProgramming
+      </a>{' '}
+      is a paradigm that was coined by David
+      Harel and others in{' '}
+      <a href="http://www.wisdom.weizmann.ac.il/~amarron/BP%20-%20CACM%20-%20Author%20version.pdf">
+        this paper
+      </a>.
+    </p>
+    <p>
+      It's a different way of programming that
+      is more aligned with how people think
+      about behavior. Specifically we program
+      using these software modules called{' '}
+      <b>b-threads</b> (aka function
+      generators) that run in parallel and can{' '}
+      <span style={{ color: 'blue' }}>
+        request
+      </span>,{' '}
+      <span style={{ color: 'green' }}>
+        wait
+      </span>{' '}
+      and{' '}
+      <span style={{ color: 'red' }}>
+        block
+      </span>{' '}
+      events.
+    </p>
+    <p>
+      <a href="https://github.com/lmatteis/react-behavioral">
+        react-behavioral
+      </a>{' '}
+      is a library, specifically targeted at
+      React, that implements this paradigm.
+    </p>
     <div>
       <ButtonContainer />
     </div>
