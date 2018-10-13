57368261700d70fde29519963fdde69d0aa32da2
Luca Matteis <lmatteis@gmail.com>
2018-10-13 11:41:11 +0200

    Add shopping example

---
+++ examples/shopping/index.js
@@ -0,0 +1,32 @@
+import 'regenerator-runtime/runtime';
+
+import React from 'react';
+
+export default () => (
+  <React.Fragment>
+    <h2>
+      An interactive essay on the development lifetime
+      of a shopping application
+    </h2>
+    <small>
+      <strong>Abstract. </strong>
+      In this essay I want to try and explore what are
+      the most critical pain points that arise during
+      the development of a complex system.
+      Specifically I will explore through the
+      development of a shopping app, that are current
+      methods are ineffective when modeling a system
+      that constantly changes. As a result we need to
+      take a more radical approach that changes how we
+      think of software development: have the
+      computers adapt to how humans generally think
+      about systems, rather than having humans adapt
+      to how computers work.
+    </small>
+    <pre>
+      <a href="https://twitter.com/lmatteis">
+        @lmatteis
+      </a>
+    </pre>
+  </React.Fragment>
+);
