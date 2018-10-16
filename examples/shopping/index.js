8e15377b4988d103aeb653f2c5e846a903c44d86
lmatteis <lmatteis@gmail.com>
2018-10-16 23:26:12 +0200

    Make shopping less scientific and more direct

--- examples/shopping/index.js
+++ examples/shopping/index.js
@@ -2,56 +2,60 @@ import 'regenerator-runtime/runtime';
 
 import React from 'react';
 
+import shoppingGif from './shopping.gif';
+
+function Box({ children }) {
+  return (
+    <div
+      style={{
+        width: '700px',
+        marginLeft: '-100px'
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
 export default () => (
   <React.Fragment>
     <h2>
-      An interactive essay on the development lifecycle of a
-      shopping application
+      Behavioral Programming: an interactive essay on the
+      development lifecycle of a shopping application
     </h2>
     <pre>
       <a href="https://twitter.com/lmatteis">
         Luca Matteis
       </a>
     </pre>
-    <small>
-      <strong>Abstract. </strong>
-      In this essay I want to try and explore what are the
-      most critical pain points that arise during the
-      development of a complex system. Specifically I will
-      explore through the development of a shopping app,
-      that our current methods are ineffective when modeling
-      a system that (i) constantly changes, and (ii) has a
-      variety of people working on it that come and go. As a
-      system becomes more complex it becomes inevitably
-      harder to maintain and modify. As a result we will ask
-      ourselves: can changes to the system not depend on its
-      complexity? In other words: how easily can we
-      implement new features without having to worry about
-      how badly the system was built? We will explore the
-      answer to this question using a paradigm named
-      "Behavioral Programming" (aka scenario-based
-      programming).
-    </small>
-    <h2>Shopping app</h2>
+
     <p>
-      To more easily understand what the{' '}
-      <i>hurdles of development</i> are we will develop a
-      shopping application similar to a simple e-commerce.
-      More specifically we will develop the app using what
-      is currently a standard library for UIs:{' '}
-      <a href="https://reactjs.org/">ReactJS</a>.{' '}
+      In this interactive essay we will be building a rather
+      complicated shopping application using a library I
+      called{' '}
+      <a href="https://github.com/lmatteis/react-behavioral">
+        react-behavioral
+      </a>.
     </p>
     <p>
-      At each code example I will present two versions: on
-      the left the currently "standard best-practices"
-      version that we're all used to; on the right the
-      "Behavioral Programming" (BP) version. As we go on
-      with the examples the way BP works will become
-      clearer.
+      If you haven't already seen the{' '}
+      <a href="/">TicTacToe example</a> built with
+      react-behavioral and showcasing how Behavioral
+      Programming works more in detail, I suggest you check
+      it out.
     </p>
     <p>
-      Without further a due here's what the app should look
-      like during a first iteration:
+      Rather than concentrating on various aspects of the
+      app we will focus all our attention on the behavior:
+      when I click this button, this should appear and these
+      3 other things should happen which in turn also make
+      this other thing happen.
+    </p>
+    <p>Here's how the shopping app looks like:</p>
+    <p>
+      <Box>
+        <img src={shoppingGif} />
+      </Box>
     </p>
   </React.Fragment>
 );
