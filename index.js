17016b15394de488856af1a6e57259d592405fab
lmatteis <lmatteis@gmail.com>
2019-02-15 14:32:54 +0100

    Readd complex example

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ import ReactDOM from 'react-dom';
 
 import TicTacToeGame from './examples/tic-tac-toe';
 import Bakery from './examples/bakery/index';
+import BakeryComplex from './examples/bakery/bakery';
 
 ReactDOM.render(
   <React.Fragment>
-    {window.location.pathname === '/bakery' ? (
-      <Bakery />
-    ) : (
-      <TicTacToeGame />
-    )}
+    {
+      {
+        '/bakery': <Bakery />,
+        '/bakery-complex': <BakeryComplex />,
+        '/': <TicTacToeGame />
+      }[window.location.pathname]
+    }
   </React.Fragment>,
   document.getElementById('content')
 );
